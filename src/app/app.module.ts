2b17bd7c344ab546624ef9dd28b9c339e32a2c24
Muhammed Thanish <mnmtanish@gmail.com>
2017-06-28 09:14:18 +0530

    Add the test-button component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TestButtonComponent } from './test-button/test-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestButtonComponent
   ],
   imports: [
     BrowserModule
